c9d2d1be448ff5245d3cbbb2801da551966030db
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-04-17 10:54:38 +0530

    Added Types

---
+++ src/Spark/MLlib/Types.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DataKinds #-}
+module Spark.MLlib.Types where
+
+import Java
+import Spark.Core
+
+data {-# CLASS "org.apache.spark.mllib.classification.ClassificationModel" #-}
+  ClassificationModel = ClassificationModel (Object# ClassificationModel)
+  deriving Class
+
+data {-# CLASS "org.apache.spark.mllib.linalg.Vector" #-}
+  Vector = Vector (Object# Vector)
+  deriving Class
+
+data {-# CLASS "org.apache.spark.mllib.classification.LogisticRegressionModel" #-}
+  LogisticRegressionModel = LogisticRegressionModel (Object# LogisticRegressionModel)
+  deriving Class
+
+data {-# CLASS "org.apache.spark.mllib.regression.GeneralizedLinearModel" #-}
+  GeneralizedLinearModel = GeneralizedLinearModel (Object# GeneralizedLinearModel)
+  deriving Class
+
+data {-# CLASS "org.apache.spark.mllib.regression.GeneralizedLinearAlgorithm" #-}
+  GeneralizedLinearAlgorithm m = GeneralizedLinearAlgorithm (Object# (GeneralizedLinearAlgorithm m))
+  deriving Class
+
+data {-# CLASS "org.apache.spark.mllib.regression.LabeledPoint" #-}
+  LabeledPoint = LabeledPoint (Object# LabeledPoint)
+  deriving Class
+
+data {-# CLASS "org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS" #-}
+  LogisticRegressionWithLBFGS = LogisticRegressionWithLBFGS (Object# LogisticRegressionWithLBFGS)
+  deriving Class

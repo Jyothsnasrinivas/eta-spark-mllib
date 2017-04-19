fd658424c0758a4b1b0214dbc81cb57c86c00fe8
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-04-19 14:29:49 +0530

    Imported MulticlassMetrics into Evaluation

--- src/Spark/MLlib/Evaluation.hs
+++ src/Spark/MLlib/Evaluation.hs
@@ -22,3 +22,39 @@ foreign import java unsafe thresholds :: Java BinaryClassificationMetrics (RDD O
 foreign import java unsafe unpersist :: Java BinaryClassificationMetrics ()
 
 -- End org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+
+-- Start org.apache.spark.mllib.evaluation.MulticlassMetrics
+
+foreign import java unsafe accuracy :: Java MulticlassMetrics Double
+
+foreign import java unsafe confusionMatrix :: Java MulticlassMetrics Matrix
+
+foreign import java unsafe falsePositiveRate :: Double -> Java MulticlassMetrics Double
+
+foreign import java unsafe fMeasure :: Double -> Java MulticlassMetrics Double
+
+foreign import java unsafe "fMeasure"
+  fMeasure :: Double -> Double -> Java MulticlassMetrics Double
+
+foreign import java unsafe labels :: Double -> Java MulticlassMetrics JDoubleArray
+
+foreign import java unsafe precision :: Double -> Java MulticlassMetrics Double
+
+foreign import java unsafe recall :: Double -> Java MulticlassMetrics Double
+
+foreign import java unsafe truePositiveRate :: Double -> Java MulticlassMetrics Double
+
+foreign import java unsafe weightedFalsePositiveRate :: Java MulticlassMetrics Double
+
+foreign import java unsafe weightedFMeasure :: Java MulticlassMetrics Double
+
+foreign import java unsafe "weightedFMeasure"
+  weightedFMeasureDouble :: Double -> Java MulticlassMetrics Double
+
+foreign import java unsafe weightedPrecision :: Java MulticlassMetrics Double
+
+foreign import java unsafe weightedRecall :: Java MulticlassMetrics Double
+
+foreign import java unsafe weightedTruePositiveRate :: Java MulticlassMetrics Double
+
+-- End org.apache.spark.mllib.evaluation.MulticlassMetrics

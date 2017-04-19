b5172106bc9f07147908798fb19406b483cbdda2
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-04-19 14:06:18 +0530

    Added Evaluation.hs

---
+++ src/Spark/MLlib/Evaluation.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE DataKinds #-}
+
+module Spark.MLlib.Evaluation where
+
+import Java
+import Java.Array
+import Java.Primitive
+import Java.Wrappers
+import Spark.MLlib.Types
+import Spark.Core

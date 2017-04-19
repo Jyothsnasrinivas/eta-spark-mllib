eb68c41e6fb77b045c104052ac6e5960c44700a2
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-04-19 14:11:56 +0530

    Imported BinaryClassificationMetrics in to Evaluation

--- src/Spark/MLlib/Evaluation.hs
+++ src/Spark/MLlib/Evaluation.hs
@@ -8,3 +8,17 @@ import Java.Primitive
 import Java.Wrappers
 import Spark.MLlib.Types
 import Spark.Core
+
+-- Start org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+
+foreign import java unsafe areaUnderPR :: Java BinaryClassificationMetrics Double
+
+foreign import java unsafe areaUnderROC :: Java BinaryClassificationMetrics Double
+
+foreign import java unsafe numBins :: Java BinaryClassificationMetrics Int
+
+foreign import java unsafe thresholds :: Java BinaryClassificationMetrics (RDD Object)
+
+foreign import java unsafe unpersist :: Java BinaryClassificationMetrics ()
+
+-- End org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
